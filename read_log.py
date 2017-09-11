6c469f1660c3566edc0e7aedb5bb6e8afd88148c
Anh Ng <nv.anh@outlook.com>
2017-09-12 01:15:24 +0700

    gitignore

--- read_log.py
+++ read_log.py
@@ -28,53 +28,56 @@ with np.load('errors.npz') as data:
     # acuracy plot =====================
     y_stack = np.row_stack((np.array(train_acuracy), np.array(val_acuracy)))
 
-    fig = plt.figure(figsize=(15, 8))
+    fig = plt.figure(figsize=(25, 8))
     ax1 = fig.add_subplot(111)
 
     ax1.plot(epochs, y_stack[0, :], label='train', color='c', marker='o')
     ax1.plot(epochs, y_stack[1, :], label='val', color='g', marker='o')
+    ax1.legend(loc=2)
 
     plt.xticks(epochs)
     plt.xlabel('epochs')
     plt.ylabel('accuracy')
 
     handles, labels = ax1.get_legend_handles_labels()
-    lgd = ax1.legend(handles, labels, loc='upper center', bbox_to_anchor=(1.15, 1))
+    lgd = ax1.legend(handles, labels, loc='upper center', bbox_to_anchor=(.1, 1))
 
     plt.savefig('acuracy.png')
 
     # loss plot =====================
     y_stack = np.row_stack((np.array(train_loss), np.array(val_loss)))
 
-    fig = plt.figure(figsize=(15, 8))
+    fig = plt.figure(figsize=(25, 8))
     ax1 = fig.add_subplot(111)
 
     ax1.plot(epochs, y_stack[0, :], label='train', color='c', marker='o')
     ax1.plot(epochs, y_stack[1, :], label='val', color='g', marker='o')
+    ax1.legend(loc=2)
 
     plt.xticks(epochs)
     plt.xlabel('epochs')
     plt.ylabel('loss')
 
     handles, labels = ax1.get_legend_handles_labels()
-    lgd = ax1.legend(handles, labels, loc='upper center', bbox_to_anchor=(1.15, 1))
+    lgd = ax1.legend(handles, labels, loc='upper center', bbox_to_anchor=(.1, 1))
 
     plt.savefig('loss.png')
 
     # jaccard plot =====================
     y_stack = np.row_stack((np.array(train_jaccard), np.array(val_jaccard)))
 
-    fig = plt.figure(figsize=(15, 8))
+    fig = plt.figure(figsize=(25, 8))
     ax1 = fig.add_subplot(111)
 
     ax1.plot(epochs, y_stack[0, :], label='train', color='c', marker='o')
     ax1.plot(epochs, y_stack[1, :], label='val', color='g', marker='o')
+    ax1.legend(loc=2)
 
     plt.xticks(epochs)
     plt.xlabel('epochs')
     plt.ylabel('mean jaccard')
 
     handles, labels = ax1.get_legend_handles_labels()
-    lgd = ax1.legend(handles, labels, loc='upper center', bbox_to_anchor=(1.15, 1))
+    lgd = ax1.legend(handles, labels, loc='upper center', bbox_to_anchor=(.1, 1))
 
     plt.savefig('jaccard.png')
